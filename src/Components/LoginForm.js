683a32e90e27e3f23fcd90813c4626a9133991df
Maria Fernanda Ojeda Carrillo <0228660@up.edu.mx>
2022-11-21 07:39:19 -0600

    updated code

--- src/Components/LoginForm.js
+++ src/Components/LoginForm.js
@@ -1,7 +1,7 @@
 import React, {useState} from 'react'
 import './LogIn.css'
 
-export const LoginForm = () => {
+export const LoginForm = ({redirect}) => {
 
   const [user, setUser] = useState(undefined)
   const [password, setPassword] = useState(undefined)
@@ -19,12 +19,12 @@ export const LoginForm = () => {
 
     <>
       <div className='loginForm'>
-        <h1>Log In</h1>
+        <h1>Bank Account</h1>
         <form>
           <label>ID: </label> <input onChange={ handleUserOnChange }/> <br/>
           <label>Password: </label> <input type="password" onChange={ handlePasswordOnChange }/> <br/>
           <button>Log in</button>
-          <p> Don't have an account? <a href="http://ww.google.com">Sign In</a> </p>
+          <p> Don't have an account? <a onClick={ redirect }>Sign In</a> </p>
         </form>
       </div>
     </>
--- src/index.js
+++ src/index.js
@@ -3,7 +3,8 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import { MainPage } from './Pages/index';
-import {SignInForm, LoginForm} from './Components/index';
+import {LoginForm} from './Components/index';
+import {SignInForm} from './Components/index';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -11,6 +12,7 @@ root.render(
   <React.StrictMode>
     
     <LoginForm/>
+    <SignInForm/>
 
   </React.StrictMode>
 );
