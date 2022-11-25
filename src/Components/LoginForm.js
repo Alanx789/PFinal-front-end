0024845d0e1a2b340d1caaa8fc3d6be73236823d
Alanx789 <sonic600@live.com.mx>
2022-11-24 20:12:42 -0600

    V3

--- src/Components/LoginForm.js
+++ src/Components/LoginForm.js
@@ -1,21 +1,33 @@
 import React, {useState} from 'react'
 import './LogIn.css'
 
-export const LoginForm = () => {
+export const LoginForm = ({redirect}) => {
 
   const [user, setUser] = useState(undefined)
   const [password, setPassword] = useState(undefined)
+  
+  const [message, setMessage] = useState(undefined)
+
+
+  const submitHandler = () => {
+    if (user == '' || user == undefined ){ message = "You must enter an user name"; return } 
+    if (password == '' || password == undefined ){ message = "You must enter a password"; return }
+
+    //Función conectado con el backend
+
+  }
 
   return (
 
     <>
-      <div  className='loginForm'>
-        <h1>Log In</h1>
-        <form>
-          <label>User: </label> <input onChange={ (e) => { setUser(e.target.value) } }/> <br/>
-          <label>Password: </label> <input onChange={ (e) => { setPassword(e.target.value) } }/> <br/>
-          <button>Log in</button>
-          <p> Don't have an account? <a href="http://ww.google.com">Sign In</a> </p>
+      <div className='loginForm'>
+        <h1>Bank Account</h1>
+        <form onSubmit={submitHandler}>
+          <label>ID: </label> <input onChange={ (e) => { setUser(e.target.value) } }/> <br/>
+          <label>Password: </label> <input type="password" onChange={ (e) => { setPassword(e.target.value) } }/> <br/>
+          <button type='submit'>Log in</button>
+          <p> Don't have an account? <a onClick={ redirect }>Sign In</a> </p>
+          <p>{message}</p>
         </form>
       </div>
     </>
--- src/Components/SignInForm.js
+++ src/Components/SignInForm.js
@@ -1,9 +1,11 @@
 import React, { useState } from 'react'
 
-export const SignInForm = () => {
+export const SignInForm = ( {redirect} ) => {
 
   //Variables del usuario
-  const [user, setUser] = useState(undefined)
+  const [name, setName] = useState(undefined)
+  const [lastname, setLastname] = useState(undefined)
+  const [phone, setPhone] = useState(undefined)
   const [password, setPassword] = useState(undefined)
   const [confirmPassword, setConfirmPassword] = useState(undefined)
 
@@ -20,16 +22,28 @@ export const SignInForm = () => {
     }
   }
 
+  const handleSubmit = () => {
+
+    verifyPassword
+    if( verification ){
+      setMessage(" Login ")
+    }
+
+  }
+
+
   return (
 
     <>
       <div>
-        <form>
-          <label>User name:  </label> <input onChange={ (e) => { setUser(e.target.value) } }/> <br/>
-          <label>New password: </label> <input onChange={ (e) => { setPassword(e.target.value) } }/> <br/>
-          <label>Confirm password: </label> <input onChange={ (e) => { setConfirmPassword(e.target.value) } }/> <br/>
-          <button>Sign in</button>
-          <p> Already have an account? <a href="http://ww.google.com">Log In</a> </p>
+        <form  onSubmit={ handleSubmit }>
+          <label>Name:  </label> <input onChange={ (e) => { setName( e.target.value ) } }/> <br/>
+          <label>Lastname:  </label> <input onChange={ (e) => { setLastname( e.target.value ) } }/> <br/>
+          <label>Phone:  </label> <input onChange={ (e) => { setPhone( e.target.value ) } }/> <br/>
+          <label>Password: </label> <input onChange={ (e) => { setPassword( e.target.value )} }/> <br/>
+          <label>Confirm password: </label> <input onChange={ (e) => { setConfirmPassword( e.target.value ) } }/> <br/>
+          <button type="submit" >Sign in</button>
+          <p> Already have an account? <a onClick={redirect}>Log In</a> </p>
           <p>{message}</p>
         </form>
       </div>
