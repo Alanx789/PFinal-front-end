63bcf1ac7d0217b6501a8906769d1c040c54ddc4
Maria Fernanda Ojeda Carrillo <0228660@up.edu.mx>
2022-11-27 11:55:55 -0600

    updated changes:(

--- src/Routes/index.js
+++ src/Routes/index.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import { BrowserRouter, Route, Routes} from 'react-router-dom'
-import { LoginForm, SignInForm } from '../Components'
+import { LoginForm, SignInForm, Transaction } from '../Components'
 import { MainPage, UserPage } from '../Pages'
 import { LogOutPage } from '../Pages/LogOutPage'
 
@@ -17,6 +17,7 @@ export const Router = () => {
                     <Route path='/sign-in' element={ <SignInForm setUserID={setUserID} setUserName={setUserName}/> }/>
                     <Route path='/log-in' element={ <LoginForm setUserID={setUserID} setUserName={setUserName}/> }/>
                     <Route path='/accounts-info/:id' element={ <UserPage id={userID}/> }/>
+                    <Route path='/transactions/:id' element={ <Transactions id={userID}/> }/>
                     <Route path='/logged-out' element={ <LogOutPage /> }/>
                 </Routes>
             </BrowserRouter>
